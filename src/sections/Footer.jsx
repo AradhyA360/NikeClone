0400f0ddd171dce3fdef0caf1c92552188fa8327
AradhyA360 <aradhyaaa@hotmail.com>
2024-04-20 15:01:22 +0530

    Subscribe Done

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,28 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} width={150} height={46} />
+          </a>
+          <p className="mt-6 text-base font-montserrat leading -7 text-slate-600 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+          <div className="flex items-center gap-5 mt-8 ">
+            {socialMedia.map((icon) => (
+              <div className="flex justify-center items-center w-1/2 h12 bg-white rounded-full">
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex-flex-1 justify-between lg:gap-10 gap-20 flex-wrap"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
